40ea70b0076e86891fdc373964fe5312f4ae156c
Aaron Hubber <aaronhubber@gmail.com>
2022-04-07 14:39:02 +0100

    extension complete

--- src/get_prefered.py
+++ src/get_prefered.py
@@ -5,3 +5,17 @@ def get_prefered(task_1, task_2):
         return task_1
     elif task_1.description == "Clean Windows" and task_2.description == "Wash Dishes":
         return task_1
+    elif task_1.description == "Wash Dishes" and task_2.description == "Wash Clothes":
+        return task_1
+    elif task_1.description == "Cook Dinner" and task_2.description == "Do Ironing":
+        return task_1
+    elif task_1.description == "Clean Windows" and task_2.description == "Do Ironing":
+        return task_1
+    elif task_1.description == "Do Ironing" and task_2.description == "Wash Clothes":
+        return task_1
+    elif task_1.description == "Do Ironing" and task_2.description == "Wash Dishes":
+        return task_1
+    elif task_1.description == "Wash Clothes" and task_2.description == "Cook Dinner":
+        return task_1
+    elif task_1.description == "Wash Clothes" and task_2.description == "Clean Windows":
+        return task_1
--- tests/task_test.py
+++ tests/task_test.py
@@ -8,6 +8,8 @@ class TestTask(unittest.TestCase):
         self.task_1 = Task("Wash Dishes", 10)
         self.task_2 = Task("Cook Dinner", 30)
         self.task_3 = Task ("Clean Windows", 60)
+        self.task_4 = Task ("Wash Clothes", 90)
+        self.task_5 = Task ("Do Ironing", 45)
 
 
     def test_wash_dish_has_description (self):
@@ -29,5 +31,32 @@ class TestTask(unittest.TestCase):
         task = get_prefered(self.task_3, self.task_1)
         self.assertEqual("Clean Windows", task.description)
     
+    def test_wash_dish_prefered_over_wash_clothes(self):
+        task = get_prefered(self.task_1, self.task_4)
+        self.assertEqual("Wash Dishes", task.description) 
+
+    def test_cook_dinner_prefered_over_do_ironing(self):
+        task = get_prefered(self.task_2, self.task_5)
+        self.assertEqual("Cook Dinner", task.description) 
+    
+    def test_clean_windows_prefered_over_do_ironing(self):
+        task = get_prefered(self.task_3, self.task_5)
+        self.assertEqual("Clean Windows", task.description) 
+    
+    def test_do_ironing_prefered_over_wash_clothes(self):
+        task = get_prefered(self.task_5, self.task_4)
+        self.assertEqual("Do Ironing", task.description) 
+    
+    def test_do_ironing_prefered_over_wash_dishes(self):
+        task = get_prefered(self.task_5, self.task_1)
+        self.assertEqual("Do Ironing", task.description) 
+    
+    def test_wash_clothes_prefered_over_cook_dinner(self):
+        task = get_prefered(self.task_4, self.task_2)
+        self.assertEqual("Wash Clothes", task.description) 
+  
+    def test_wash_clothes_prefered_over_clean_windows(self):
+        task = get_prefered(self.task_4, self.task_3)
+        self.assertEqual("Wash Clothes", task.description) 
 
     
